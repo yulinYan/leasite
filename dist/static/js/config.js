3cbf4ab7ec431b96f16fdec16ddcae46183b9cd3
unknown <xhhuang@dh.com>
2019-12-21 17:22:37 +0800

    add

--- dist/static/js/config.js
+++
@@ -1,7 +0,0 @@
-(function(){
-	window.configs={
-	leansiteAPI:"http://192.168.2.59:9527/",//leansite Paas 平台
-	internetAPI:"http://192.168.2.59:9001/",//物联网中心
-	appPlatformAPI:"http://192.168.2.59:8880/"//运维中心
-};
-})();
