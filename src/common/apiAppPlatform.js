9e198a5c0cd02ee664d4622ac14df35bba976f4c
unknown <xhhuang@dh.com>
2019-12-26 11:28:30 +0800

    打包最新版本的前端

--- src/common/apiAppPlatform.js
+++ src/common/apiAppPlatform.js
@@ -4,7 +4,7 @@ import axios from 'axios'
  */
 const interface_s = {
 	baseURL: 'http://192.168.2.59:8880/',//非生产环境
-	//prodBaseURL:window.configs.appPlatformAPI,//生产环境
+	prodBaseURL:window.configs.appPlatformAPI,//生产环境
 };
 //配置生产环境和非生产环境之间的切换
 if(process.env.NODE_ENV == 'development') { //非生产环境
